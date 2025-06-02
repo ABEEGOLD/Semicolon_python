133c08b8c64ff12a8a9bba8b8986598063e7fc4a
ABEEGOLD <abeeutee@gmail.com>
2025-06-02 17:06:41 +0100

    Phase Gate day 2

---
+++ Phase Gate day 2/RandomQuestion.py
@@ -0,0 +1,59 @@
+
+
+
+def randomNumbers(numbers):
+
+	 firstNumber = Math.floor(Math.random() * 100)
+	 secondNumber = Math.floor(Math.random() * 100)
+	
+	 subtractionOperator = 0
+
+		
+	 if firstNumber > secondNumber:
+			subtractionOperator = firstNumber - secondNumber;
+			print(subtractionOperator)
+				
+			elif firstNumber < secondNumber:
+				 scorelow = secondNumber
+				 subtractionOperator = secondNumber - scorelow
+				 print(secondNumber+ "-" +scorelow)
+		        			
+		
+				 return subtractionOperator
+
+
+
+count = 0
+
+for i in range(1,10):
+	print("question"+ i)
+
+	 Attempt = randomNumbers();
+	userAnswer = print("What is your answer?")
+
+	if userAnswer == randomNumbers:
+		count++		
+	
+	
+	elif userAnswer != randomNumbers
+		print("second try")
+		print("What is your answer?")
+	
+
+
+	
+print("The score is  "+ count + "/10")
+
+	
+
+
+
+
+
+	
+	
+	
+
+
+
+
